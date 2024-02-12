9379b8cc55b45bb8d0769ecd46b0f7823c5b42a5
Yuliana <pashkova.yuliana@gmail.com>
2024-02-12 15:14:12 +0200

    add url for checkout, dish detail, delete from cart, add to cart, orders pages in urls.py

--- bufet_system/urls.py
+++ bufet_system/urls.py
@@ -1,12 +1,27 @@
 from django.urls import path
 
-from bufet_system.views import index, MenuListView, ProductCartListView, RestaurantListView
+from bufet_system.views import (MenuListView,
+                                ProductCartListView,
+                                OrderCreateView,
+                                DishDetailView,
+                                RestaurantListView,
+                                AddToCartView,
+                                DeleteFromCartView, OrderListView, IndexView)
 
 urlpatterns = [
-    path("", index, name="index"),
+    path("", IndexView.as_view(), name="index"),
     path("menu/", MenuListView.as_view(), name="menu"),
     path("product_cart/", ProductCartListView.as_view(), name="product-cart"),
+    path("checkout/", OrderCreateView.as_view(), name="order-checkout"),
+    path("dish/<int:pk>/", DishDetailView.as_view(), name="dish-detail"),
+    path("delete-from-cart/<int:dish_id>/",
+         DeleteFromCartView.as_view(),
+         name="delete-from-cart"),
     path("restaurants/", RestaurantListView.as_view(), name="restaurant-list"),
+    path("add-to-cart/<int:dish_id>/",
+         AddToCartView.as_view(),
+         name="add-to-cart"),
+    path("orders/", OrderListView.as_view(), name="order-list")
 ]
 
 app_name = "bufet_system"
