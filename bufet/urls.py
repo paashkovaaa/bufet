b75cb24e6dbd1a66ed2af6c5fa44a062038fe105
Yuliana <pashkova.yuliana@gmail.com>
2024-02-12 15:10:26 +0200

    refactor urls.py

--- bufet/urls.py
+++ bufet/urls.py
@@ -7,4 +7,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("bufet_system.urls", namespace="bufet")),
     path("accounts/", include('django.contrib.auth.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
