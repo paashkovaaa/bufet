4918006644fe32d7daa7f1f960cb3efacf1f2782
Yuliana <pashkova.yuliana@gmail.com>
2024-02-16 16:32:23 +0200

    add bufet-website.onrender.com to allowed hosts in settings.py

--- bufet/settings.py
+++ bufet/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'bufet-website.onrender.com']
 
 # Application definition
 
